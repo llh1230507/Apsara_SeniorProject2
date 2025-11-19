720ea532704e1f6ad35dad02260350fa8309b2a7
LeangHour Ly <u6530342@au.edu>
2025-11-19 10:30:58 +0700

    second commit

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDuT6Jw5yCo2Gogprx01F-8AT6t3LY3AVc",
+  authDomain: "apsara-dd748.firebaseapp.com",
+  projectId: "apsara-dd748",
+  storageBucket: "apsara-dd748.firebasestorage.app",
+  messagingSenderId: "721600353027",
+  appId: "1:721600353027:web:da2771a9c629710fe6cf36"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
